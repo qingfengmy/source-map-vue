c000bd96896b95993735373acab3961e09e9dbd0
qingfengmy <qingfengmy@users.noreply.github.com>
2025-09-25 08:43:43 +0000

    deploy: 388a87a34fe6253373cec1f8959ef26f8bc833d8

---
+++ assets/About-CNWVbPoR.js
@@ -0,0 +1,2 @@
+import{e as n,c as a,o as c,d as s,a as l,F as r}from"./index-m-aNjCGB.js";const p=n({__name:"About",setup(u){const t=()=>{console.log("click",e)};return(i,o)=>(c(),a(r,null,[o[0]||(o[0]=s(" About ",-1)),l("button",{onClick:t},"click")],64))}});export{p as default};
+//# sourceMappingURL=About-CNWVbPoR.js.map
