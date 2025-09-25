871842657a856d0bab7d2abc22ae5df8619aa5d2
zhangtao <zhangtao@tingyutech.com>
2025-09-25 10:15:02 +0800

    feat: init

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: './',
+  build: {
+    sourcemap: false,
+  },
+  plugins: [vue()],
+})
